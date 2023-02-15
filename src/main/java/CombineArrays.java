248e60ee102d3b82e301541f3d3694e7a2771912
Devynn R. Palmer <palmerdevynn@gmail.com>
2023-02-15 22:08:55 +0000

    combine arrays lab

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,16 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int[] arr3 = new int[arr1.length + arr2.length];
+        int arr3Index = 0;
+        for(int i = 0; i < arr1.length; i++) {
+            arr3[arr3Index] = arr1[i];
+            arr3Index++;
+        }
+        for(int i = 0; i < arr2.length; i++) {
+            arr3[arr3Index] = arr2[i];
+            arr3Index++;
+        }
+        return arr3;
     }
 }
